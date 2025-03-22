775f720bf261cd13d0e4dc2c8a3ef23725b67dff
Zachary Krueger <ZacharyKrueger321@gmail.com>
2025-03-21 20:10:44 -0500

    Implemented Djikstra efficiently using minimum priority queue

--- dkstra.py
+++ dkstra.py
@@ -18,6 +18,8 @@ Thomas H. Cormen, et al. Introduction to Algorithms, Fourth Edition. The MIT Pre
 APA 7th Edition (American Psychological Assoc.)
 Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, &amp; Clifford Stein. (2022). Introduction to Algorithms, Fourth Edition: Vol. Fourth edition. The MIT Press.'''
 
+import heapq
+                
 def getMinIdx(l, seen):
     minVal = float('inf')
     minI = 0
@@ -40,7 +42,7 @@ def dijkstra(graph, src):
     for i in range(len(graph)):
         non_visited_nodes.add(i)
         
-    nodes_visited = []
+    nodes_visited = set()
     
     while len(non_visited_nodes) > 0:
             
@@ -48,7 +50,7 @@ def dijkstra(graph, src):
         cur_node = getMinIdx(dist, nodes_visited)
         
         # store that we've visited it
-        nodes_visited.append(cur_node)
+        nodes_visited.add(cur_node)
         non_visited_nodes.remove(cur_node) # remove it form the set
         
         for dst, w in graph[cur_node].items(): # for each edge (dst and weight) at cur_node
@@ -56,7 +58,36 @@ def dijkstra(graph, src):
             if dist[cur_node] + w < dist[dst]: 
                 dist[dst] = dist[cur_node] + w
     
-    return dist
+    return dist    
+
+def dijkstra2(graph, src):
+    
+    # the distances, initialize all to inf except src node
+    dists = [float('inf') for _ in range(len(graph))]
+    dists[src] = 0
+    
+    Q = []
+    heapq.heappush(Q, [0, src]) # push only valid distance (src) into the minimum priority queue
+    
+    # set storing which nodes have not been visite
+        
+    nodes_visited = set()
+    
+    while len(Q) > 0:
+        # get minimum of the currently unvisited nodes
+        dist, cur_node = heapq.heappop(Q) # grab the node with the minimum distance and distance up to that node
+                
+        # store that we've visited it
+        nodes_visited.add(cur_node) # add this node to our visited set
+     
+        for dst, w in graph[cur_node].items(): # for each edge (dst and weight) at cur_node
+            # if the distance to current node + weight of edge is less than the preivous shorteset distance to dst overwrite it
+            if dist + w < dists[dst]: 
+                dists[dst] = dists[cur_node] + w  # rewrite as before
+                heapq.heappush(Q, [dists[dst], dst]) # if we've rewrote this, then we have a new shortest path we need to account for
+    
+    return dists    
+
 
 # Example graph represented as an adjacency list with weights.
 graph = {
@@ -67,6 +98,6 @@ graph = {
     4: {}
 }
 
-print(dijkstra(graph, 0))
+print(dijkstra2(graph, 0))
 
 
